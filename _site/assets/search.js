1739315cb3c5494e1476dc0bf55ee7f1364f148c
gusgkr0117 <gusgkr0117@naver.com>
2021-01-31 20:16:39 +0900

    update logo

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"Montgomery Multiplication",'url':"/2021/01/31/Montgomery-Multiplication.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2021/01/30/welcome.html"},{'title':"Montgomery Multiplication",'url':"/2021/01/31/Montgomery-Multiplication.html"}]};
