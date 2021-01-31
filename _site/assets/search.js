831baa0760af6693a011271e5dc373794d363c9c
gusgkr0117 <gusgkr0117@naver.com>
2021-01-31 17:25:18 +0900

    Update posts

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"Montgomery Multiplication",'url':"/2021/01/31/Montgomery-Multiplication.html"}]};
